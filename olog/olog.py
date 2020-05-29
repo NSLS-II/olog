345bebae29a51d20bfca71e9a688d02e3c7efc3d
ke-zhang-rd <kezhang@bnl.gov>
2020-05-29 13:10:04 -0400

    MNT: Move util function/class to util.py

--- olog/olog.py
+++ olog/olog.py
@@ -1,43 +1,9 @@
 import asyncio
 from collections import OrderedDict
-from datetime import date, datetime
 
 from httpx import AsyncClient
 
-__all__ = ['Client']
-
-_TS_FORMATS = [
-    '%Y-%m-%d %H:%M:%S.%f',
-    '%Y-%m-%d %H:%M:%S',
-    '%Y-%m-%d %H:%M',  # these 2 are not as originally doc'd,
-    '%Y-%m-%d %H',     # but match previous pandas behavior
-    '%Y-%m-%d',
-    '%Y-%m',
-    '%Y']
-
-
-def ensure_time(time):
-    if isinstance(time, datetime):
-        pass
-    elif isinstance(time, date):
-        time = datetime.fromisoformat(time.isoformat())
-    elif isinstance(time, float) or isinstance(time, int):
-        time = datetime.fromtimestamp(time)
-    else:
-        for fmt in _TS_FORMATS:
-            try:
-                time = datetime.strptime(time, fmt)
-                break
-            except ValueError:
-                pass
-        else:
-            raise ValueError("Your time is in wrong format.")
-
-    return time.isoformat(sep=' ', timespec='milliseconds')
-
-
-class UncaughtServerError(Exception):
-    pass
+from .util import UncaughtServerError, ensure_time
 
 
 class Client:
